31a2005e28b7479bea96df47babba03936e0f79c
Rohit Adolla <arohit5529@gmail.com>
2025-06-24 17:50:18 +0530

    ✅ Fix: remove rollupOptions.input for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: '.',                            // Explicitly set root
   plugins: [react()],
-  build: {
-    outDir: 'dist',                     // Default output
-    rollupOptions: {
-      input: './index.html',            // 👈 This line fixes the error
-    },
-  },
+  server: {
+    port: 5173
+  }
 });
